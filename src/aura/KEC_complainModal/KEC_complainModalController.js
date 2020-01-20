2b52215e30adb6f3faf8e0d6ea7575a490f60784
sandrakuc <san.kuc.96@gmail.com>
2020-01-20 11:22:28 +0100

    prepare complain modal

---
+++ src/aura/KEC_complainModal/KEC_complainModalController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by BRITENET on 20.01.2020.
+ */
+({
+    closeModal: function(component, event, helper){
+        helper.close(component);
+    }
+})
---
+++ src/aura/KEC_complainModal/KEC_complainModalHelper.js
@@ -0,0 +1,8 @@
+/**
+ * Created by BRITENET on 20.01.2020.
+ */
+({
+    close: function(component){
+        component.set("v.openModal", false);
+    }
+})
