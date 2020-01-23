5deab17d5765b6dca2329d8b6197910443cc6f6f
sandrakuc <san.kuc.96@gmail.com>
2020-01-23 13:57:19 +0100

    add setting product main photo

--- src/aura/KEC_changeMainPhoto/KEC_changeMainPhotoHelper.js
+++ src/aura/KEC_changeMainPhoto/KEC_changeMainPhotoHelper.js
@@ -84,5 +84,45 @@
                 $A.util.addClass(photos[i], "chosenPhoto");
             }
         }
+    },
+    setPhoto: function(component, event){
+        let index = event.currentTarget.dataset.id;
+        let photos = component.get("v.photos");
+        let mainPhoto = photos[index];
+        let url = mainPhoto.ImgUrl__c;
+        let recordId = component.get("v.recordId");
+        let action = component.get("c.setMainPhoto");
+        action.setParams({
+            url : url,
+            productId : recordId
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS"){
+                 let operationResult = response.getReturnValue(),
+                     title = operationResult.isSuccess ? $A.get("$Label.c.KEC_Success") : $A.get("$Label.c.KEC_Error"),
+                     type = operationResult.isSuccess ? "success" : "error",
+                     message = operationResult.message;
+                 component.find("toastCmp").toast(title, type, message);
+                 component.set("v.openModal", false);
+                 this.setClass(component, index);
+            }
+            else if (state === "ERROR"){
+                 let errors = response.getError();
+                 let message,
+                      title = $A.get("$Label.c.KEC_Error");
+                 if (errors){
+                      if (errors[0] && errors[0].message){
+                           message = errors[0].message;
+                           component.find("toastCmp").toast(title, "error", message);
+                      }
+                 }
+                 else{
+                      message = $A.get("$Label.c.KEC_UnknownError");
+                      component.find("toastCmp").toast(title, "error", message);
+                 }
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
