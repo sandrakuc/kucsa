cd6783e6f48c227433b1109c0957dfdf3a8173a5
sandrakuc <san.kuc.96@gmail.com>
2020-01-23 13:53:58 +0100

    hide cases button for order with no cases

--- src/aura/KEC_changeMainPhoto/KEC_changeMainPhotoController.js
+++ src/aura/KEC_changeMainPhoto/KEC_changeMainPhotoController.js
@@ -4,5 +4,8 @@
 ({
     onInit: function(component, event, helper){
         helper.getPhotos(component);
+    },
+    setNewMainPhoto: function(component, event, helper){
+        helper.setPhoto(component, event);
     }
 })
